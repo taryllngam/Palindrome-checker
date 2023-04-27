a035ba17454eb5968e742d84469a6a5c26742527
NGAM TARYLL <taryllngam@gmail.com>
2023-04-27 12:23:35 +0100

    added functionality with js

---
+++ script.js
@@ -0,0 +1,21 @@
+const inputArea = document.getElementById('input');
+const btn = document.getElementById('btn');
+const display = document.getElementById('display')
+
+
+const isPalindrome = () => {
+    const input = inputArea.value
+    const normalizedText = input.replace(/[^a-zA-Z]/g, "").toLowerCase();
+    const reverse = normalizedText.split("").reverse().join("");
+    if(input === ""){
+        alert("Please enter a word to check")
+        display.innerHTML = 'No word to check'
+    }
+    if(normalizedText === reverse){
+        display.innerHTML = `${input}, is a palindrome`
+    }else {
+        display.innerHTML = `${input}, is not a palindrome`
+    }
+}
+
+btn.addEventListener('click', isPalindrome)
